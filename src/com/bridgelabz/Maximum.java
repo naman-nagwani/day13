e9e4e46daa854df170b4b2e1360e0754f88fd5a8
“naman-nagwani” <namannagwani@gmail.com>
2022-08-20 21:42:37 +0530

    UC5 added : PrintMax

--- src/com/bridgelabz/Maximum.java
+++ src/com/bridgelabz/Maximum.java
@@ -8,19 +8,23 @@ public class Maximum <T extends Comparable<T>> {
 
 
 
-    public static <T extends Comparable<T>> void testMaximum(T first, T second, T third) {
+    public static <T extends Comparable<T>> T testMaximum(T first, T second, T third) {
         if(first.compareTo(second) >= 0 && first.compareTo(third) >= 0) {
             System.out.println(" First is largest ");
+            return first;
         }
         else if(second.compareTo(first) >= 0 && second.compareTo(third) >= 0) {
             System.out.println(" Second is largest");
+            return second;
         }
         else
             System.out.println(" Third is largest");
+        return third;
     }
 
     public void testMaximum() {
-        testMaximum(this.first, this.second, this.third);
+        T max = testMaximum(this.first, this.second, this.third);
+        printMax(max);
     }
 
     public Maximum(T first, T second, T third) {
@@ -28,5 +32,9 @@ public class Maximum <T extends Comparable<T>> {
         this.first = first;
         this.second = second;
         this.third = third;
+    }
+
+    public void printMax(T max) {
+        System.out.println(" max = " + max);
     }
 }
