fc70d5fd7e34b7cc53def278c7198fcf2c92f1bc
“naman-nagwani” <namannagwani@gmail.com>
2022-08-20 17:11:33 +0530

    refactor 1 added generics

--- src/com/bridgelabz/MaximumMain.java
+++ src/com/bridgelabz/MaximumMain.java
@@ -4,20 +4,64 @@ import java.util.Scanner;
 
 public class MaximumMain {
     public static void main(String[] args) {
+        // TODO Auto-generated method stub
         Scanner scan = new Scanner(System.in);
         Maximum obj = new Maximum();
 
-        System.out.print(" Please enter the first value: ");
-        String first = scan.next();
 
-        System.out.print(" Please enter the second value: ");
-        String second = scan.next();
+        while(true) {
+            System.out.println(" 1. Integers\n 2. Float\n 3. String\n 4. Exit");
+            System.out.print(" Please enter your choice: ");
+            int ch = scan.nextInt();
 
-        System.out.print(" Please enter the third value: ");
-        String third = scan.next();
+            switch(ch) {
+                case 1:
+                {
+                    System.out.print(" Please enter the first value: ");
+                    Integer first = scan.nextInt();
 
-        obj.findMaxString(first, second, third);
+                    System.out.print(" Please enter the second value: ");
+                    Integer second = scan.nextInt();
 
-        scan.close();
+                    System.out.print(" Please enter the third value: ");
+                    Integer third = scan.nextInt();
+                    obj.findMax(first, second, third);
+                    break;
+                }
+                case 2:
+                {
+                    System.out.print(" Please enter the first value: ");
+                    Float first = scan.nextFloat();
+
+                    System.out.print(" Please enter the second value: ");
+                    Float second = scan.nextFloat();
+
+                    System.out.print(" Please enter the third value: ");
+                    Float third = scan.nextFloat();
+                    obj.findMax(first, second, third);
+                    break;
+                }
+                case 3:
+                {
+                    System.out.print(" Please enter the first value: ");
+                    String first = scan.next();
+
+                    System.out.print(" Please enter the second value: ");
+                    String second = scan.next();
+
+                    System.out.print(" Please enter the third value: ");
+                    String third = scan.next();
+                    obj.findMax(first, second, third);
+                    break;
+                }
+                case 4:
+                    System.out.println(" Good bye");
+                    scan.close();
+                    return;
+                default:
+                    System.out.println("Please enter a valid input");
+            }
+
+        }
     }
 }
